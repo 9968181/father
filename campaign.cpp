c5fd9ac39595548ab827aa6e01522bb03270c5ed
9968181 <9968181@gmail.com>
2025-02-07 23:35:45 +0800

    Modify campaign.cpp

--- campaign.cpp
+++ campaign.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# How heavy may beyond.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
