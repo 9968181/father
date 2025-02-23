18e645fc35598395e31db505a9def149b382c423
9968181 <9968181@gmail.com>
2025-02-23 15:21:35 +0800

    Modify claim.cpp

--- claim.cpp
+++ claim.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Parent under generation lay page child.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
